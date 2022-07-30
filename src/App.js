98d879433c58547ed7749c7b065350b24969e61f
MshSiam <siyamkenway@gmail.com>
2022-07-30 23:43:27 +0600

    plan done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css"
 import Hero from "./components/hero/Hero"
+import Plan from "./components/plan/Plan"
 import Programs from "./components/programs/Programs"
 import Reasons from "./components/reasons/Reasons"
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Hero></Hero>
       <Programs></Programs>
       <Reasons></Reasons>
+      <Plan></Plan>
     </div>
   )
 }
---
+++ src/components/plan/Plan.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { plansData } from "../../data/plansData"
+import whiteTik from "../../assets/whiteTick.png"
+import css from "./plan.css"
+
+const Plan = () => {
+  return (
+    <div className="plans-container">
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">ARE YOU READY</span>
+        <span>TO START YOUR JOURNEY</span>
+        <span className="stroke-text">WITH US ?</span>
+      </div>
+      <div className="plans-card">
+        <div className="plans">
+          {plansData.map((plan, i) => {
+            return (
+              <div key={i} className="plan">
+                {plan.icon}
+                <span>{plan.name}</span>
+                <span>${plan.price}</span>
+                <div className="features">
+                  {plan.features.map((feature, i) => {
+                    return (
+                      <div className="feature">
+                        <img src={whiteTik} alt="" />
+                        <span key={i}>{feature}</span>
+                      </div>
+                    )
+                  })}
+                </div>
+
+                <button id="btn" className="btn">
+                  Join Now
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Plan
