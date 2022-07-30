6a58727616fa62fa594586135247481d8e4445d5
MshSiam <siyamkenway@gmail.com>
2022-07-30 08:06:26 +0600

    hero done

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -54,16 +54,31 @@ const Hero = () => {
           <button className="btn">Get Started</button>
           <button className="btn">Learn More</button>
         </div>
-
-        {/* right side of the hero */}
       </div>
+      {/* right side of the hero */}
+
       <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heartRate">
+        <div className="heart-rate">
           <img src={Hearts} alt="" />
           <span>Heart Rate</span> <span>116 bpm</span>
         </div>
+        {/* hero image */}
+        <img src={hero_image} className="hero-image" alt="hero image" />
+        <img
+          src={hero_image_back}
+          className="hero-image-back"
+          alt="hero image back"
+        />
+
+        {/* calories div */}
+        {/* <div className="calories">
+          <img src={calories} alt="" />
+          <div>
+            <span>Calories Burn</span> <span>220 KCal</span>
+          </div>
+        </div> */}
       </div>
     </div>
   )
